e7c399b5cbaa29336509ff2672b5bd0db281bc01
Eric Stagl <StaglBagl@gmail.com>
2018-10-27 11:01:54 -0500

    add babel

---
+++ src/index.js
@@ -0,0 +1,94 @@
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+
+import Issue from '../models/Issue';
+
+const app = express();  // Init App
+const router = express.Router(); // Init Router
+
+app.use(cors());
+
+//Mongo Database
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+    console.log('Successfully connected to database');
+});
+
+//Body Parser
+//let bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+// Static Folder
+app.use(express.static(__dirname + '/public/dist'));
+mongoose.connect('mongodb://localhost:27017/Issues', { useNewUrlParser: true });
+
+
+router.route('/issues').get((req, res) => {
+    Issue.find((err, issues) => {
+        if (err)
+            console.log(err);
+        else
+            res.json(issues);
+    });
+});
+
+router.route('/issues/:id').get((req, res) => {
+    Issue.findById(req.params.id, (err, issue) => {
+        if (err)
+            console.log(err);
+        else
+            res.json(issue);
+    });
+});
+
+router.route('/issues/add').post((req, res) => {
+    let issue = new Issue(req.body);
+    issue.save()
+        .then(issue => {
+            res.status(200).json({'issue': 'Added successfully'});
+        })
+        .catch(err => {
+            res.status(400).send('Failed to create new record');
+        });
+});
+
+router.route('/issues/update/:id').post((req, res) => {
+    Issue.findById(req.params.id, (err, issue) => {
+        if (!issue)
+            return next(new Error('Could not load document'));
+        else {
+            issue.title = req.body.title;
+            issue.resp = req.body.resp;
+            issue.desc = req.body.desc;
+            issue.severity = req.body.severity;
+            issue.status = req.body.status;
+
+            issue.save().then(issue => {
+                res.json('Update done');
+            }).catch(err => {
+                res.status(400).send('Update failed');
+            });
+        }
+    });
+});
+
+router.route('/issues/delete/:id').get((req, res) => {
+    Issue.findByIdAndRemove({_id: req.params.id}, (err, issue) => {
+        if (err)
+            res.json(err);
+        else
+            res.json('Remove successfully');
+    })
+})
+
+
+// Routes
+app.use('/', router);
+
+// Server Listening @ 4000
+var port = 4000
+app.listen(port, () => console.log('Express server running on port '+ port));
